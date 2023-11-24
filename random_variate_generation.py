d6be8c35ed069db20c4d244543920376b03dbec1
Kelly Lee <kellychin79@gmail.com>
2023-11-23 21:11:06 -0500

    geomtric and poisson distribution

--- random_variate_generation.py
+++ random_variate_generation.py
@@ -6,6 +6,7 @@
 
 import random
 import matplotlib.pyplot as plt
+import math
 
 
 # ## Bernoulli distribution
@@ -29,6 +30,8 @@ def generate_prn():
 # In[37]:
 
 
+# generate x bernoulli random variates i.e. trials with probability p
+
 def generate_bernoulli_rv(p, x):
     outputs = []
     prns = []
@@ -45,7 +48,7 @@ def generate_bernoulli_rv(p, x):
     return outputs, prns
 
 
-# In[59]:
+# In[127]:
 
 
 def draw_bernoulli_rv(p, x):
@@ -56,6 +59,8 @@ def draw_bernoulli_rv(p, x):
     
     plt.title(label='p = '+str(p)+', x = '+str(x))
     plt.bar(x=x_axis, height=y_axis)
+    
+    return rv_outputs
 
 
 # In[54]:
@@ -64,22 +69,47 @@ def draw_bernoulli_rv(p, x):
 bernoulli_p = 0.2
 
 
-# In[60]:
+# In[141]:
+
+
+# small sample size
+bernoulli_rv_outputs = draw_bernoulli_rv(p=bernoulli_p, x=1)
+
+
+# In[142]:
+
+
+bernoulli_rv_outputs
+
+
+# In[130]:
 
 
 # small sample size
 bernoulli_size = 10
 
-draw_bernoulli_rv(p=bernoulli_p, x=bernoulli_size)
+bernoulli_rv_outputs = draw_bernoulli_rv(p=bernoulli_p, x=bernoulli_size)
 
 
-# In[61]:
+# In[131]:
+
+
+bernoulli_rv_outputs
+
+
+# In[132]:
 
 
 # big sample size
 bernoulli_size = 1000
 
-draw_bernoulli_rv(p=bernoulli_p, x=bernoulli_size)
+bernoulli_rv_outputs = draw_bernoulli_rv(p=bernoulli_p, x=bernoulli_size)
+
+
+# In[133]:
+
+
+bernoulli_rv_outputs
 
 
 # ## Binomial distribution
@@ -96,6 +126,8 @@ draw_bernoulli_rv(p=bernoulli_p, x=bernoulli_size)
 # In[111]:
 
 
+# generate x binomial random variates of bernoulli trials with probability p
+
 def generate_binomial_rv(p, x):
     outputs = []
     
@@ -108,7 +140,7 @@ def generate_binomial_rv(p, x):
     return outputs
 
 
-# In[112]:
+# In[136]:
 
 
 def draw_binomial_rv(p, x):
@@ -129,6 +161,8 @@ def draw_binomial_rv(p, x):
         values.append(i)
         probs.append(rv_outputs.count(i)/total_num)
     ax[1].bar(x=values, height=probs)
+    
+    return rv_outputs
 
 
 # In[118]:
@@ -137,22 +171,28 @@ def draw_binomial_rv(p, x):
 binomial_p = 0.7
 
 
-# In[125]:
+# In[137]:
 
 
 # small sample size
 binomial_size = 20
 
-draw_binomial_rv(p=binomial_p, x=binomial_size)
+binomial_rv_outputs = draw_binomial_rv(p=binomial_p, x=binomial_size)
 
 
-# In[126]:
+# In[138]:
+
+
+binomial_rv_outputs
+
+
+# In[139]:
 
 
 # big sample size
 binomial_size = 1000
 
-draw_binomial_rv(p=binomial_p, x=binomial_size)
+binomial_rv_outputs = draw_binomial_rv(p=binomial_p, x=binomial_size)
 
 
 # In[91]:
@@ -161,81 +201,244 @@ draw_binomial_rv(p=binomial_p, x=binomial_size)
 draw_binomial_rv(p=binomial_p, x=binomial_size, y_axis='probability')
 
 
-# In[ ]:
+# In[140]:
 
 
+binomial_rv_outputs
 
 
+# ## Geometric distribution
 
-# ## Binomial distribution
-
-# - Statistical meaning: For an experiment (trial) that gives only 'Success' and 'Failure' outcome, the number of ‘Success’ in a sequence of *n* experiments (aka trials).
+# - Statistical meaning: The probability of having *X* Bernoulli trials with probability *p* until a Success occurs, i.e. the first success.
 
 # - Real-life examples: 
-#     - Flip a coin: the number of heads and tails in X times (aka trial, experiments)  
-#     - Charity asks for donations: the number donations in a month
+#     - Job search: how many applications until receiving an offer
+#     - Defect products: how many items produced until the first defective unit
 
 # - Graphs:
-# ![image-3.png](attachment:image-3.png)
+# ![image-4.png](attachment:image-4.png)
 
-# In[111]:
+# In[221]:
 
 
-def generate_binomial_rv(p, x):
+def geometric_pmf(p):
+    outputs = []
+    failure_count = 0
+    
+    while failure_count < 10:
+        outputs.append((1-p)**failure_count*p)
+        failure_count += 1
+        
+    return outputs, failure_count
+
+
+# In[222]:
+
+
+def draw_geometric_distribution(p):
+    rv_outputs, trial_count = geometric_pmf(p)
+
+    plt.title(label='p = '+str(p))
+    plt.ylabel('probablity')
+    values = [i for i in range(trial_count)]
+    plt.plot(values, rv_outputs)
+
+
+# In[223]:
+
+
+geometric_p = 0.2
+draw_geometric_distribution(p=geometric_p)
+
+
+# In[224]:
+
+
+geometric_p = 0.8
+draw_geometric_distribution(p=geometric_p)
+
+
+# In[225]:
+
+
+draw_geometric_distribution(p=0.2)
+draw_geometric_distribution(p=0.6)
+draw_geometric_distribution(p=0.8)
+
+
+# In[280]:
+
+
+# generate x geometric random variate of bernoulli trials with probability p
+
+def generate_geometric_rv(p, x):
+    outputs = []
+    trial_counts = []
+    
+    for i in range(x):
+        bernoulli_outcome = 0
+        trial_count = 0
+        
+        while bernoulli_outcome != 1: # keep going until a Success occurs
+            bernoulli_outcome = generate_bernoulli_rv(p, 1)[0][0]
+            trial_count += 1
+
+        outputs.append((1-p)**(trial_count-1)*p)
+        trial_counts.append(trial_count) 
+        
+    return outputs, trial_counts
+
+
+# In[290]:
+
+
+def draw_geometric_rv(p, x):
+    rv_outputs, trial_counts = generate_geometric_rv(p, x)
+
+    plt.title(label='p = '+str(p))
+    values = [i for i in trial_counts]
+    plt.bar(values, rv_outputs)
+    
+    return trial_counts
+
+
+# In[284]:
+
+
+geometric_p = 0.2
+
+
+# In[293]:
+
+
+# small sample size
+geometric_size = 10
+
+geometric_rv_outputs = draw_geometric_rv(p=geometric_p, x=geometric_size)
+
+
+# In[294]:
+
+
+geometric_rv_outputs
+
+
+# In[295]:
+
+
+# big sample size
+geometric_size = 1000
+
+geometric_rv_outputs = draw_geometric_rv(p=geometric_p, x=geometric_size)
+
+
+# In[296]:
+
+
+geometric_rv_outputs
+
+
+# ## Poisson distribution
+
+# - Statistical meaning: The probability of an event happening a certain number of times *k* within a given interval of time or space. 
+
+# - Real-life examples: 
+#     - Call Center: the probability that the call center receives more than 5 phone calls during the noon, given the average 3 calls per hour during that time period.
+#     - Broadcast: the probablity that the news reporter says "uh" more than three times during a broadcast, given the average 2 "uh" per broadcast.
+
+# - Graphs:
+# ![image-6.png](attachment:image-6.png)
+
+# In[338]:
+
+
+# generate x poisson random variate in one time unit
+
+def generate_poisson_rv(l, x):
     outputs = []
     
     for i in range(x):
-        # the number of successes and failures out of Bernoulli trials
-        bernoulli_output = generate_bernoulli_rv(p, x)[0] 
-        # the number of successes (=1)
-        outputs.append(bernoulli_output.count(1)) 
+        prn = generate_prn()
+        time_unit = 0 
+        cmf = 0
+        while prn > cmf:
+            pmf = math.e**-l*l**time_unit/math.factorial(time_unit)
+            cmf = cmf + pmf
+            time_unit += 1
+        outputs.append(time_unit)    
         
     return outputs
 
 
-# In[112]:
+# In[372]:
 
 
-def draw_binomial_rv(p, x):
-    rv_outputs = generate_binomial_rv(p, x)
+def draw_poisson_rv(l, x):
+    rv_outputs = generate_poisson_rv(l, x)
+
+    plt.title(label='lambda = '+str(l))
     
-    fig, ax = plt.subplots(1,2)
-
-    ax[0].set_title(label='p = '+str(p)+', x = '+str(x))
-    ax[0].set_ylabel('count')
-    ax[0].hist(x=rv_outputs)
-    
-    ax[1].set_title(label='p = '+str(p)+', x = '+str(x))
-    ax[1].set_ylabel('probablity')
     values = []
-    probs = []
-    total_num = len(rv_outputs)
-    for i in rv_outputs:
+    counts = []
+    for i in set(rv_outputs):
         values.append(i)
-        probs.append(rv_outputs.count(i)/total_num)
-    ax[1].bar(x=values, height=probs)
+        counts.append(rv_outputs.count(i))
+
+    plt.plot(values, counts)
+    
+    return rv_outputs
 
 
-# In[118]:
+# In[378]:
 
 
-binomial_p = 0.7
+poisson_lambda = 2
 
 
-# In[125]:
+# In[380]:
 
 
 # small sample size
-binomial_size = 20
+lambda_size = 10
 
-draw_binomial_rv(p=binomial_p, x=binomial_size)
+poisson_rv_outputs = draw_poisson_rv(l=poisson_lambda, x=lambda_size)
 
 
-# In[126]:
+# In[381]:
+
+
+poisson_rv_outputs
+
+
+# In[382]:
 
 
 # big sample size
-binomial_size = 1000
+lambda_size = 1000
 
-draw_binomial_rv(p=binomial_p, x=binomial_size)
+poisson_rv_outputs = draw_poisson_rv(l=poisson_lambda, x=lambda_size)
+
+
+# In[383]:
+
+
+poisson_rv_outputs
+
+
+# In[385]:
+
+
+lambda_size = 1000
+
+poisson_lambda = 2
+poisson_rv_outputs = draw_poisson_rv(l=poisson_lambda, x=lambda_size)
+
+poisson_lambda = 4
+poisson_rv_outputs = draw_poisson_rv(l=poisson_lambda, x=lambda_size)
+
+
+# In[ ]:
+
+
+
 
