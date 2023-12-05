1854eec6a6efc89cbbf6adc810f4b9f96fff4dea
Kelly Lee <kellychin79@gmail.com>
2023-12-04 22:37:07 -0500

    added proper labels to charts

--- random_variate_generation.py
+++ random_variate_generation.py
@@ -54,19 +54,23 @@ def generate_uniform_rv(n, x):
     return outputs
 
 
-# In[4]:
+# In[431]:
 
 
 def draw_uniform_rv(n, x):
     rv_outputs = generate_uniform_rv(n, x)
     
-    fig, ax = plt.subplots(1,2)
-
-    ax[0].set_title(label='n outcomes = '+str(n)+', x = '+str(x))
-    ax[0].set_ylabel('count')
-    ax[0].hist(x=rv_outputs)
+    fig, ax = plt.subplots(1,2, figsize=(6, 3))
+    fig.tight_layout(pad=3.0)
     
-    ax[1].set_title(label='n outcomes = '+str(n)+', x = '+str(x))
+    ax[0].set_title(label='# of outcomes = '+str(n)+'; x = '+str(x))
+    ax[0].set_xlabel('outomce value')
+    ax[0].set_ylabel('count')
+    ax[0].hist(x=rv_outputs, bins=np.arange(7)+0.5, rwidth=0.8)
+    ax[0].set_xticks([1,2,3,4,5,6])
+    
+    ax[1].set_title(label='# of outcomes = '+str(n)+'; x = '+str(x))
+    ax[1].set_xlabel('outomce value')
     ax[1].set_ylabel('probablity')
     values = []
     probs = []
@@ -75,17 +79,18 @@ def draw_uniform_rv(n, x):
         values.append(i)
         probs.append(rv_outputs.count(i)/total_num)
     ax[1].bar(x=values, height=probs)
+    ax[1].set_xticks([1,2,3,4,5,6])
     
     return rv_outputs
 
 
-# In[5]:
+# In[432]:
 
 
 uniform_outcomes = 6
 
 
-# In[6]:
+# In[433]:
 
 
 # small sample size
@@ -94,19 +99,13 @@ uniform_size = 20
 uniform_rv_outputs = draw_uniform_rv(n=uniform_outcomes, x=uniform_size)
 
 
-# In[7]:
+# In[434]:
 
 
 uniform_rv_outputs
 
 
-# In[5]:
-
-
-uniform_outcomes = 6
-
-
-# In[13]:
+# In[435]:
 
 
 # big sample size
@@ -115,7 +114,7 @@ uniform_size = 1000
 uniform_rv_outputs = draw_uniform_rv(n=uniform_outcomes, x=uniform_size)
 
 
-# In[14]:
+# In[436]:
 
 
 print(uniform_rv_outputs)
@@ -132,7 +131,7 @@ print(uniform_rv_outputs)
 # - Graphs:
 # ![image-2.png](attachment:image-2.png)
 
-# In[16]:
+# In[65]:
 
 
 # generate x bernoulli random variates i.e. trials with probability p
@@ -153,7 +152,7 @@ def generate_bernoulli_rv(p, x):
     return outputs, prns
 
 
-# In[17]:
+# In[84]:
 
 
 def draw_bernoulli_rv(p, x):
@@ -162,32 +161,35 @@ def draw_bernoulli_rv(p, x):
     x_axis = ['0', '1']
     y_axis = [rv_outputs.count(0), rv_outputs.count(1)]
     
+    plt.figure(figsize=(4,3))    
     plt.title(label='p = '+str(p)+', x = '+str(x))
     plt.bar(x=x_axis, height=y_axis)
+    plt.xlabel('outomce value')
+    plt.ylabel('count')
     
     return rv_outputs
 
 
-# In[32]:
+# In[63]:
 
 
 bernoulli_p = 0.2
 
 
-# In[33]:
+# In[85]:
 
 
 # small sample size
 bernoulli_rv_outputs = draw_bernoulli_rv(p=bernoulli_p, x=1)
 
 
-# In[34]:
+# In[83]:
 
 
 bernoulli_rv_outputs
 
 
-# In[30]:
+# In[86]:
 
 
 # small sample size
@@ -196,13 +198,13 @@ bernoulli_size = 10
 bernoulli_rv_outputs = draw_bernoulli_rv(p=bernoulli_p, x=bernoulli_size)
 
 
-# In[31]:
+# In[87]:
 
 
 print(bernoulli_rv_outputs)
 
 
-# In[24]:
+# In[88]:
 
 
 # big sample size
@@ -211,7 +213,7 @@ bernoulli_size = 1000
 bernoulli_rv_outputs = draw_bernoulli_rv(p=bernoulli_p, x=bernoulli_size)
 
 
-# In[25]:
+# In[89]:
 
 
 print(bernoulli_rv_outputs)
@@ -228,7 +230,7 @@ print(bernoulli_rv_outputs)
 # - Graphs:
 # ![image-3.png](attachment:image-3.png)
 
-# In[35]:
+# In[90]:
 
 
 # generate x binomial random variates of bernoulli trials with probability p
@@ -245,20 +247,27 @@ def generate_binomial_rv(p, x):
     return outputs
 
 
-# In[36]:
+# In[172]:
 
 
 def draw_binomial_rv(p, x):
     rv_outputs = generate_binomial_rv(p, x)
     
-    fig, ax = plt.subplots(1,2)
+    fig, ax = plt.subplots(1,2, figsize=(8, 4))
+    fig.tight_layout(pad=3.0)    
 
-    ax[0].set_title(label='p = '+str(p)+', x = '+str(x))
+    ax[0].set_title(label='Bernoulli trial prob = '+str(p)+', x = '+str(x))
     ax[0].set_ylabel('count')
-    ax[0].hist(x=rv_outputs)
+    ax[0].set_xlabel('# of Success')
+    min_rv = min(rv_outputs)
+    bin_ct = max(rv_outputs) - min_rv + 1
+    ax[0].hist(x=rv_outputs, 
+               bins=np.arange(bin_ct+1)+min_rv-0.5, 
+               rwidth=0.8)
     
-    ax[1].set_title(label='p = '+str(p)+', x = '+str(x))
+    ax[1].set_title(label='Bernoulli trial prob = '+str(p)+', x = '+str(x))
     ax[1].set_ylabel('probablity')
+    ax[1].set_xlabel('# of Success')
     values = []
     probs = []
     total_num = len(rv_outputs)
@@ -267,16 +276,21 @@ def draw_binomial_rv(p, x):
         probs.append(rv_outputs.count(i)/total_num)
     ax[1].bar(x=values, height=probs)
     
+    xticks = [min(rv_outputs)+i for i in range(bin_ct)]
+    if len(xticks) < 15:
+        ax[0].set_xticks(xticks)    
+        ax[1].set_xticks(xticks)    
+    
     return rv_outputs
 
 
-# In[37]:
+# In[92]:
 
 
 binomial_p = 0.7
 
 
-# In[38]:
+# In[169]:
 
 
 # small sample size
@@ -285,13 +299,19 @@ binomial_size = 20
 binomial_rv_outputs = draw_binomial_rv(p=binomial_p, x=binomial_size)
 
 
-# In[40]:
+# In[160]:
 
 
 print(binomial_rv_outputs)
 
 
-# In[41]:
+# In[180]:
+
+
+binomial_p = 0.46
+
+
+# In[178]:
 
 
 # big sample size
@@ -300,7 +320,7 @@ binomial_size = 1000
 binomial_rv_outputs = draw_binomial_rv(p=binomial_p, x=binomial_size)
 
 
-# In[43]:
+# In[181]:
 
 
 print(binomial_rv_outputs)
@@ -365,7 +385,7 @@ draw_geometric_distribution(p=0.6)
 draw_geometric_distribution(p=0.8)
 
 
-# In[44]:
+# In[182]:
 
 
 # generate x geometric random variate of bernoulli trials with probability p
@@ -388,26 +408,29 @@ def generate_geometric_rv(p, x):
     return outputs, trial_counts
 
 
-# In[45]:
+# In[204]:
 
 
 def draw_geometric_rv(p, x):
     rv_outputs, trial_counts = generate_geometric_rv(p, x)
 
-    plt.title(label='p = '+str(p))
+    plt.figure(figsize=(6,3))
+    plt.title(label='Bernoulli trials prob = '+str(p))
     values = [i for i in trial_counts]
     plt.bar(values, rv_outputs)
+    plt.xlabel('# of Bernoulli Trials until the first Success')
+    plt.ylabel('Probability')
     
     return trial_counts
 
 
-# In[46]:
+# In[205]:
 
 
 geometric_p = 0.2
 
 
-# In[47]:
+# In[210]:
 
 
 # small sample size
@@ -416,13 +439,13 @@ geometric_size = 10
 geometric_rv_outputs = draw_geometric_rv(p=geometric_p, x=geometric_size)
 
 
-# In[48]:
+# In[211]:
 
 
 print(geometric_rv_outputs)
 
 
-# In[49]:
+# In[208]:
 
 
 # big sample size
@@ -431,7 +454,7 @@ geometric_size = 1000
 geometric_rv_outputs = draw_geometric_rv(p=geometric_p, x=geometric_size)
 
 
-# In[50]:
+# In[209]:
 
 
 print(geometric_rv_outputs)
@@ -448,7 +471,7 @@ print(geometric_rv_outputs)
 # - Graphs:
 # ![image-6.png](attachment:image-6.png)
 
-# In[51]:
+# In[212]:
 
 
 # generate x poisson random variate within a time period
@@ -469,12 +492,13 @@ def generate_poisson_rv(l, x):
     return outputs
 
 
-# In[52]:
+# In[225]:
 
 
 def draw_poisson_rv(l, x):
     rv_outputs = generate_poisson_rv(l, x)
-
+    
+    plt.figure(figsize=(5,3))
     plt.title(label='lambda = '+str(l))
     
     values = []
@@ -482,19 +506,22 @@ def draw_poisson_rv(l, x):
     for i in set(rv_outputs):
         values.append(i)
         counts.append(rv_outputs.count(i))
-
+    
+    
     plt.plot(values, counts)
+    plt.xlabel('# of events')
+    plt.ylabel('count')
     
     return rv_outputs
 
 
-# In[53]:
+# In[226]:
 
 
 poisson_lambda = 2
 
 
-# In[54]:
+# In[228]:
 
 
 # small sample size
@@ -503,13 +530,13 @@ lambda_size = 10
 poisson_rv_outputs = draw_poisson_rv(l=poisson_lambda, x=lambda_size)
 
 
-# In[55]:
+# In[229]:
 
 
 print(poisson_rv_outputs)
 
 
-# In[56]:
+# In[232]:
 
 
 # big sample size
@@ -518,28 +545,39 @@ poisson_size = 1000
 poisson_rv_outputs = draw_poisson_rv(l=poisson_lambda, x=poisson_size)
 
 
-# In[57]:
+# In[218]:
 
 
 print(poisson_rv_outputs)
 
 
-# In[60]:
+# In[245]:
 
 
-poisson_size = 1000
+all_lambdas = [1, 2, 4, 10]
+legend_values = []
+sample_size = 1000
 
-poisson_lambda = 1
-poisson_rv_outputs = draw_poisson_rv(l=poisson_lambda, x=poisson_size)
+plt.figure(figsize=(5,3))
+plt.title(label='lambda = '+str(all_lambdas)
+                 +', x = '+str(sample_size))
+    
+for l in all_lambdas:
+    legend_values.append('λ = '+str(l))
+    rv_outputs = generate_poisson_rv(l, x=sample_size)
 
-poisson_lambda = 2
-poisson_rv_outputs = draw_poisson_rv(l=poisson_lambda, x=poisson_size)
+    values = []
+    counts = []
+    for i in set(rv_outputs):
+        values.append(i)
+        counts.append(rv_outputs.count(i))
 
-poisson_lambda = 4
-poisson_rv_outputs = draw_poisson_rv(l=poisson_lambda, x=poisson_size)
-
-poisson_lambda = 10
-poisson_rv_outputs = draw_poisson_rv(l=poisson_lambda, x=poisson_size)
+    plt.plot(values, counts)
+    
+plt.legend(legend_values, loc ="upper right")     
+plt.xlabel('# of events')
+plt.ylabel('count')
+plt.show()
 
 
 # ## Exponential distribution
@@ -553,7 +591,7 @@ poisson_rv_outputs = draw_poisson_rv(l=poisson_lambda, x=poisson_size)
 # - Graphs: (Y axis is basically lambda, the area below the curve is the probability)
 # ![image-11.png](attachment:image-11.png)
 
-# In[61]:
+# In[249]:
 
 
 # generate x exponential random variate between two consecutive events
@@ -578,13 +616,14 @@ def generate_exponential_rv(l, x):
     return outputs
 
 
-# In[62]:
+# In[267]:
 
 
 def draw_exponential_rv(l, x):
     rv_outputs = generate_exponential_rv(l, x)
-
-    plt.title(label='lambda = '+str(l))
+    
+    plt.figure(figsize=(5,3))
+    plt.title(label='lambda = '+str(l)+', x = '+str(x))
     
     values = []
     counts = []
@@ -592,21 +631,20 @@ def draw_exponential_rv(l, x):
         values.append(i)
         counts.append(rv_outputs.count(i))
     
-    if x <= 10:
-        plt.bar(x=values, height=counts)
-    else:
-        plt.plot(values, counts)
+    plt.bar(x=values, height=counts)
+    plt.xlabel('# of time unit elapsed between events')
+    plt.ylabel('count')
     
     return rv_outputs
 
 
-# In[63]:
+# In[268]:
 
 
 exponential_lambda = 1
 
 
-# In[64]:
+# In[273]:
 
 
 # small sample size
@@ -616,13 +654,13 @@ exponential_rv_outputs = draw_exponential_rv(l=exponential_lambda,
                                              x=exponential_size)
 
 
-# In[65]:
+# In[274]:
 
 
 print(exponential_rv_outputs)
 
 
-# In[66]:
+# In[271]:
 
 
 # big sample size
@@ -632,13 +670,13 @@ exponential_rv_outputs = draw_exponential_rv(l=exponential_lambda,
                                              x=exponential_size)
 
 
-# In[68]:
+# In[272]:
 
 
 print(exponential_rv_outputs)
 
 
-# In[590]:
+# In[275]:
 
 
 exponential_lambda = 0.5
@@ -646,19 +684,34 @@ exponential_size = 10
 d = draw_exponential_rv(l=exponential_lambda, x=exponential_size)
 
 
-# In[69]:
+# In[281]:
 
 
-exponential_size = 1000
+all_lambdas = [0.25, 0.5, 1, 1.5]
+legend_values = []
+sample_size = 1000
 
-exponential_lambda = 0.5
-d = draw_exponential_rv(l=exponential_lambda, x=exponential_size)
+plt.figure(figsize=(8,4))
+plt.title(label='lambda = '+str(all_lambdas)
+                 +', x = '+str(sample_size))
+    
+for l in all_lambdas:
+    legend_values.append('λ = '+str(l))
+    rv_outputs = generate_exponential_rv(l, 
+                                         x=sample_size)
 
-exponential_lambda = 1
-d = draw_exponential_rv(l=exponential_lambda, x=exponential_size)
+    values = []
+    counts = []
+    for i in set(rv_outputs):
+        values.append(i)
+        counts.append(rv_outputs.count(i))
 
-exponential_lambda = 1.5
-d = draw_exponential_rv(l=exponential_lambda, x=exponential_size)
+    plt.plot(values, counts)
+    
+plt.legend(legend_values, loc ="upper right")     
+plt.xlabel('# of time unit elasped between events')
+plt.ylabel('count')
+plt.show()
 
 
 # ## Weibull distribution
@@ -692,7 +745,7 @@ for k in ks:
 
 
 
-# In[70]:
+# In[284]:
 
 
 # generate x weibull random variate between two consecutive events
@@ -712,13 +765,15 @@ def generate_weibull_rv(l, x, k):
     return outputs
 
 
-# In[71]:
+# In[345]:
 
 
 def draw_weibull_rv(l, x, k):
     rv_outputs = generate_weibull_rv(l, x, k)
-
-    plt.title(label='lambda = '+str(l)+';k = '+str(k))
+    
+    plt.figure(figsize=(5, 3))
+    plt.title(label='lambda = '+str(l)+'; k = '+str(k)
+                    +'; x = '+str(x))
     
     values = []
     counts = []
@@ -726,12 +781,21 @@ def draw_weibull_rv(l, x, k):
         values.append(i)
         counts.append(rv_outputs.count(i))
     
-    plt.hist(rv_outputs)
+    max_rv = math.ceil(max(rv_outputs))
+    min_rv = 0
+    plt.hist(rv_outputs, bins=np.arange(max_rv+1), rwidth=0.8)
+    plt.xlabel('# of time unit elasped between events')
+    plt.ylabel('count')
+    
+    if max_rv > 15:
+        plt.xticks([i for i in range(1, max_rv+1, 2)])
+    else:
+        plt.xticks([i for i in range(1, max_rv+1, 1)])
     
     return rv_outputs
 
 
-# In[72]:
+# In[346]:
 
 
 weibull_lambda = 1
@@ -743,13 +807,14 @@ weibull_rv_outputs = draw_weibull_rv(l=weibull_lambda,
                                      k=weibull_k)
 
 
-# In[73]:
+# In[347]:
 
 
-print(weibull_rv_outputs)
+for i in range(15):
+    print(weibull_rv_outputs[i])
 
 
-# In[74]:
+# In[348]:
 
 
 weibull_lambda = 1
@@ -761,13 +826,14 @@ weibull_rv_outputs = draw_weibull_rv(l=weibull_lambda,
                                      k=weibull_k)
 
 
-# In[75]:
+# In[349]:
 
 
-print(weibull_rv_outputs)
+for i in range(15):
+    print(weibull_rv_outputs[i])
 
 
-# In[76]:
+# In[350]:
 
 
 weibull_lambda = 1
@@ -779,26 +845,11 @@ weibull_rv_outputs = draw_weibull_rv(l=weibull_lambda,
                                      k=weibull_k)
 
 
-# In[77]:
+# In[351]:
 
 
-print(weibull_rv_outputs)
-
-
-# In[701]:
-
-
-weibull_size = 1000
-weibull_lambda = 1
-
-weibull_k = 0.5 # failure decreases with time
-weibull_rv_outputs = draw_weibull_rv(l=weibull_lambda, x=weibull_size, k=weibull_k)
-
-weibull_k = 1 # failure constant with time
-weibull_rv_outputs = draw_weibull_rv(l=weibull_lambda, x=weibull_size, k=weibull_k)
-
-weibull_k = 2 # failure increases with time
-weibull_rv_outputs = draw_weibull_rv(l=weibull_lambda, x=weibull_size, k=weibull_k)
+for i in range(15):
+    print(weibull_rv_outputs[i])
 
 
 # ## Normal distribution
@@ -813,7 +864,7 @@ weibull_rv_outputs = draw_weibull_rv(l=weibull_lambda, x=weibull_size, k=weibull
 # - Graphs: 
 # ![image-16.png](attachment:image-16.png)
 
-# In[78]:
+# In[352]:
 
 
 # generate x normal random variate with mean and variance
@@ -830,12 +881,13 @@ def generate_normal_nv(mu, variance, x):
     return outputs
 
 
-# In[79]:
+# In[409]:
 
 
 def draw_normal_rv(mu, variance, x):
     rv_outputs = generate_normal_nv(mu, variance, x)
-
+    
+    plt.figure(figsize=(5, 3))
     plt.title(label='mu = '+str(mu)+';variance = '+str(variance))
     
     values = []
@@ -844,46 +896,75 @@ def draw_normal_rv(mu, variance, x):
         values.append(i)
         counts.append(rv_outputs.count(i))
     
-    plt.hist(rv_outputs)
+    min_rv = math.floor(min(rv_outputs))
+    max_rv = math.ceil(max(rv_outputs))
+    
+    plt.hist(rv_outputs, bins=max_rv-min_rv, rwidth=0.8)
+    plt.xlabel('x')
+    plt.ylabel('count')
     
     return rv_outputs
 
 
-# In[80]:
+# In[410]:
 
 
 normal_mu = 3
 normal_variance = 16
 
 
-# In[81]:
+# In[411]:
 
 
 # small sample size
 normal_size = 10
 
-normal_rv_outputs = draw_normal_rv(mu=normal_mu, variance=normal_variance, x=normal_size)
+normal_rv_outputs = draw_normal_rv(mu=normal_mu, 
+                                   variance=normal_variance
+                                   , x=normal_size)
 
 
-# In[83]:
+# In[412]:
 
 
-print(normal_rv_outputs)
+for i in range(10):
+    print(normal_rv_outputs[i])
 
 
-# In[84]:
+# In[414]:
 
 
 # big sample size
 normal_size = 1000
 
-normal_rv_outputs = draw_normal_rv(mu=normal_mu, variance=normal_variance, x=normal_size)
+normal_rv_outputs = draw_normal_rv(mu=normal_mu, 
+                                   variance=normal_variance, 
+                                   x=normal_size)
 
 
-# In[85]:
+# In[415]:
 
 
-print(normal_rv_outputs)
+for i in range(10):
+    print(normal_rv_outputs[i])
+
+
+# In[423]:
+
+
+# large sample size
+normal_size = 6000
+
+normal_rv_outputs = draw_normal_rv(mu=normal_mu, 
+                                   variance=normal_variance, 
+                                   x=normal_size)
+
+
+# In[424]:
+
+
+for i in range(10):
+    print(normal_rv_outputs[i])
 
 
 # In[ ]:
